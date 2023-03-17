79e61c4dc936c81b5df17c2f9c8c8a660a65bb85
DerrickSarpong <derrick.sarpong.ds@gmail.com>
2023-03-16 19:27:05 -0700

    Displaying an output with put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * Entry point for main function
+ *
+ * Return 0 if successful
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
