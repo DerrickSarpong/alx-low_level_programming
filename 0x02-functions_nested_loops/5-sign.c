96238c7ba27f6ee35798243d032a86756080cf95
DerrickSarpong <derrick.sarpong.ds@gmail.com>
2023-03-21 18:24:33 -0700

    Signe dintegers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - Check main
+ * @n: An input character
+ * Description: Returns 1 if n is greater than 0,
+ * 0 if n is 0, -1 if n is less than 0.
+ * Return: 1 , 0 or -1 .
+ */
+
+int print_sign(int n)
+{
+	int val;
+
+	if ( n > 0)
+	{
+		val = 1;
+		_putchar('+');
+	}
+	else if ( n == 0)
+	{
+		val = 0;
+		_putchar('0');
+	}
+	else
+	{
+		val = -1;
+		_putchar('-');
+	}
+	
+	return (val);
+}	
