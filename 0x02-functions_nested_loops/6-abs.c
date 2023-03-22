caea30a29505905df9ef3d29274cad51f3d24450
DerrickSarpong <derrick.sarpong.ds@gmail.com>
2023-03-21 18:39:05 -0700

    Finding absolute of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Check main
+ *@r: Integer input 
+ * Description: Prints the absolute of an integer.
+ * Return: Abolute value of integer r.
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+		return (r * -1);
+}
