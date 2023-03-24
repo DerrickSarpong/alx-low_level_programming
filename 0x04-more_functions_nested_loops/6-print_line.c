d066fb25e26539d41f405b792673a1480ccb9e65
DerrickSarpong <derrick.sarpong.ds@gmail.com>
2023-03-23 20:06:38 -0700

    Drwing lines in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - Draws a sraight line in terminal
+*/
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
