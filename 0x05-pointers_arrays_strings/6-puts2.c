7b6b4f8d0fc83f7ea75ea4524fb8817753e92f9f
DerrickSarpong <derrick.sarpong.ds@gmail.com>
2023-03-27 19:58:52 -0700

    Printing every other number in string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,12 @@ void puts2(char *str)
 	int i = 0, len = 0;
 
 	while (str[len])
-		len++;
-	
+		len++;	
 	len -= 1;
 
-	for  (i = 0; i < len; i += 2)
+	for  (i = 0; i <= len; i += 2)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
-			
