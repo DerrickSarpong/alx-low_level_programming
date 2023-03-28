638160ee40df777a8aa73b9342edf1c24538b7e7
DerrickSarpong <derrick.sarpong.ds@gmail.com>
2023-03-27 19:21:02 -0700

    Reversing a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
 
 void print_rev(char *s)
 {
-	int len = 0, i=0;
+	int len = 0, i = 0;
 
 	while (s[i++])
 		len++;
 
-	for (i = len -1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
